7d3238272765529bf9567e23a1cadabb382b27cc
masaya <haiq1020@icloud.com>
2022-07-08 12:49:43 +0900

    SignUpのDB接続試し

--- SignUp/index.ts
+++ SignUp/index.ts
@@ -3,13 +3,13 @@ import { SignUpApi } from "../library/api/signUp";
 import { AccountRepository } from "../library/db";
 
 const httpTrigger: SignUpApi = async (context, req) => {
-  const email = req.query?.email ?? req.body.email;
-  const password = req.query?.password ?? req.body.password;
+  // const email = req.query?.email ?? req.body.email;
+  // const password = req.query?.password ?? req.body.password;
   const responseMessage = "200";
 
   const repo = new AccountRepository();
 
-  User.register(repo, email, password);
+  User.register(repo, "email", "password");
 
   return {
     body: {
--- library/db.ts
+++ library/db.ts
@@ -1,11 +1,24 @@
 import { Avatar, Player, User } from "cello-core/core";
 import { IAccountRepository, Identifier } from "cello-core/infrastructure";
+import mongoose, { Schema, model, connect } from "mongoose";
+
+const UserSchema = new Schema({ email: String, password: String }, { timestamps: true });
+
+const UserModel = model("User", UserSchema);
 
 export class AccountRepository implements IAccountRepository {
   getUser(id: Identifier): Promise<User> {
     throw new Error("Method not implemented.");
   }
-  registerNewUser(user: unknown): Promise<void> {
+  registerNewUser(user: User): Promise<void> {
+    mongoose.connect(
+      "mongodb://ih4ciw4cadmin:ikZvgiIvLijUii4MfeJoXVmvfVCZZSqNtKNAkdLtCAMSjgzfKvXDGwK7T5nzIyXbZMfGanDDhchkI4twgT1xiA==@ih4ciw4cadmin.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@ih4ciw4cadmin"
+    );
+    const modelToInsert = new UserModel();
+    modelToInsert["email"] = user.email;
+    modelToInsert["password"] = user.password;
+
+    modelToInsert.save();
     throw new Error("Method not implemented.");
   }
   getUserPassword(id: Identifier): Promise<string> {
