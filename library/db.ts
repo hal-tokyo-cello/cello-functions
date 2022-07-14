725f10013a2697c1c5729832748b9e1eb0f05a6b
kaisr31sh62 <kai.sr.31.2001@gmail.com>
2022-07-14 12:31:13 +0900

    スキーマの追加

--- library/db.ts
+++ library/db.ts
@@ -6,19 +6,43 @@ mongoose.connect(
   "mongodb://ih4ciw4cadmin:ikZvgiIvLijUii4MfeJoXVmvfVCZZSqNtKNAkdLtCAMSjgzfKvXDGwK7T5nzIyXbZMfGanDDhchkI4twgT1xiA==@ih4ciw4cadmin.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@ih4ciw4cadmin"
 );
 
-const UserSchema = new mongoose.Schema({ email: String, password: String }, { timestamps: true });
+const UserSchema = new mongoose.Schema({
+  id: Number,
+  name: String,
+  email: String,
+  password: String,
+  create_date: Number,
+  update_date: Number,
+  leave_date: Number,
+  last_login: Number,
+  mail_verify: Boolean,
+});
 const QuestsSchema = new Schema({
   name: String,
   category: Number,
   exp_get: Number,
 });
+const AvatarSchema = new Schema({
+  name: String,
+  category: Number,
+  exp_get: Number,
+});
+
 const UserModel = mongoose.model("User", UserSchema);
 const QuestsModel = mongoose.model("Quests", QuestsSchema);
+const AvatarModel = mongoose.model("Avatar", AvatarSchema);
 
 const modelToInsert = new UserModel();
+// const modelToInsert = new QuestsModel();
+// const modelToInsert = new AvatarModel();
 
 export class AccountRepository implements IAccountRepository {
   getUser(id: Identifier): Promise<User> {
+    // try {
+    //   return Promise.resolve("");
+    // } catch {
+    //   return Promise.reject("理由");
+    // }
     throw new Error("Method not implemented.");
   }
   registerNewUser(user: User): Promise<void> {
@@ -34,30 +58,69 @@ export class AccountRepository implements IAccountRepository {
   }
 
   getUserPassword(id: Identifier): Promise<string> {
-    throw new Error("Method not implemented.");
+    try {
+      return Promise.resolve("");
+    } catch {
+      return Promise.reject("理由");
+    }
   }
   updateUserPassword(user: unknown, password: unknown): Promise<void> {
-    throw new Error("Method not implemented.");
+    try {
+      return Promise.resolve();
+    } catch {
+      return Promise.reject("理由");
+    }
   }
   getPlayer(id: Identifier): Promise<Player> {
+    // try {
+    //   return Promise.resolve();
+    // } catch {
+    //   return Promise.reject("理由");
+    // }
     throw new Error("Method not implemented.");
   }
   getAvatar(player: Identifier): Promise<Avatar> {
+    // try {
+    //   return Promise.resolve();
+    // } catch {
+    //   return Promise.reject("理由");
+    // }
     throw new Error("Method not implemented.");
   }
   setLastLoginAttempt(timestamp: number): Promise<void> {
-    throw new Error("Method not implemented.");
+    try {
+      return Promise.resolve();
+    } catch {
+      return Promise.reject("理由");
+    }
   }
   setLastLogin(timestamp: number): Promise<void> {
-    throw new Error("Method not implemented.");
+    try {
+      return Promise.resolve();
+    } catch {
+      return Promise.reject("理由");
+    }
   }
   upgradeUserToPlayer(user: User, player: Player): Promise<void> {
-    throw new Error("Method not implemented.");
+    try {
+      return Promise.resolve();
+    } catch {
+      return Promise.reject("理由");
+    }
   }
   unregisterUser(id: Identifier): Promise<void> {
-    throw new Error("Method not implemented.");
+    try {
+      return Promise.resolve();
+    } catch {
+      return Promise.reject("理由");
+    }
   }
   unregisterPlayer(id: Identifier): Promise<void> {
+    try {
+      return Promise.resolve();
+    } catch {
+      return Promise.reject("理由");
+    }
     throw new Error("Method not implemented.");
   }
 }
